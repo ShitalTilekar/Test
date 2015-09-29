aa60af9b78f033448de1e694f6a60ff0e72ee296
ShitalTilekar <tilekarsk15.it@coep.ac.in>
2015-09-29 20:32:35 +0530

    FILES ADDED !

---
+++ string1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char string1[200],string2[100];
+int i;
+printf("Enter two words that are to be compared\n");
+gets(string1);
+gets(string2);
+i=strcmp(string1,string2);
+if (i==0)
+printf("They are equal \n");
+else
+printf("They are not equal \n");
+return 0;
+}
+
